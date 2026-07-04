451ffc22e01ec9976fd2a30c8fb7f89c4483381c
DavidHuijser <davidhuijser@gmail.com>
2026-07-04 17:52:33 +1200

    model D

--- fit_nonlinear.R
+++ fit_nonlinear.R
@@ -3,20 +3,20 @@ fit_nonlinear <- function(x, y, start_par) {
   n <- start_par$n
   model_type <- start_par$model_type 
   print(paste("Model type", model_type))
-  if (model_type == 'A')
+  if (model_type == 'D')
   {
-      init_par <- c(start_par$a, start_par$b, start_par$c, start_par$A, start_par$B, start_par$freq)
+      init_par <- c(start_par$c, start_par$A, start_par$B, start_par$freq)
   }
   
   objective_function <- function(par) {
-    y_pred <- predict_model_A(x, par)
+    y_pred <- predict_model_D(x, par)
     sum((y - y_pred)^2)
   }
   
   result <- optim(init_par, objective_function)
-  if (model_type == 'A')
+  if (model_type == 'D')
   {
-    result$model <- predict_model_A(x, result$par)
+    result$model <- predict_model_D(x, result$par)
   }
   result
   
